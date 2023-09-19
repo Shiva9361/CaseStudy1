872dbb554d24fe3fb1c9331f3cf3ceaedad3a5f7
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-19 17:37:45 +0530

    Modified Ticket.java

---
+++ Ticket.java
@@ -0,0 +1,5 @@
+public class Ticket {
+    String customerId;//null if customer is not registered
+    int phoneNumber;// if customer is not registered then phone number shall be used 
+    int entryTime;
+}
