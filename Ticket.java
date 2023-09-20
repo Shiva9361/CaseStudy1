a1a2a499d8b537f64e5d6f22b7d8d6df583129f5
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-21 00:35:44 +0530

    Added data storage support using arraylist

--- Ticket.java
+++ Ticket.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+import java.util.UUID; // TO create unique id for tickets
 public class Ticket {
+    UUID ticketId;
     String customerId;//null if customer is not registered
     String phoneNumber;// if customer is not registered then phone number shall be used 
     long entryTime;
     
-    Ticket(String customerId,String phoneNumber){
+    Ticket(String customerId,String phoneNumber,ArrayList<Ticket> Tickets){
+        this.ticketId = UUID.randomUUID();
         this.customerId=customerId;
         this.phoneNumber=phoneNumber;
         this.entryTime = System.currentTimeMillis();
-        
+        Tickets.add(this);
     }
-    Ticket(String phoneNumber){
+    Ticket(String phoneNumber,ArrayList<Ticket> Tickets){
+        this.ticketId = UUID.randomUUID();
         this.phoneNumber=phoneNumber;
         this.entryTime= System.currentTimeMillis();
+        Tickets.add(this);
     }
 }
