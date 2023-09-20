58d62167abf47fd1cb5f72cd2f1056c0e7861dd2
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-20 22:14:03 +0530

    Added Constructor

--- Ticket.java
+++ Ticket.java
@@ -1,8 +1,16 @@
 public class Ticket {
     String customerId;//null if customer is not registered
-    int phoneNumber;// if customer is not registered then phone number shall be used 
-    int entryTime;
-    Ticket(){
+    String phoneNumber;// if customer is not registered then phone number shall be used 
+    long entryTime;
+    
+    Ticket(String customerId,String phoneNumber){
+        this.customerId=customerId;
+        this.phoneNumber=phoneNumber;
+        this.entryTime = System.currentTimeMillis();
         
+    }
+    Ticket(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+        this.entryTime= System.currentTimeMillis();
     }
 }
