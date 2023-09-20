4fa8627f5a02e98b929c6b003cb881464706db78
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-20 22:13:16 +0530

    Switched to arraylist

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Customer[] customerData = new Customer[3];
+        ArrayList<Customer> customerData = new ArrayList<Customer>();
         // data
+        customerData.add(new Customer("a", "9999999999", null, 0, 0,"aa"));
+        customerData.add(new Customer("b", "8888888888", null, 0, 0,"bb"));
+        customerData.add(new Customer("c", "0000000000", null, 0, 0,"cc"));
+        /*
         customerData[0] = new Customer("a", "9999999999", null, 0, 0,"aa");
         customerData[1] = new Customer("b", "8888888888", null, 0, 0,"bb");
         customerData[2] = new Customer("c", "0000000000", null, 0, 0,"cc");// add some id
-
+        */
+        ArrayList<Ticket> tickets = new ArrayList<Ticket>();
         //CustomerInfoPortal cip = new CustomerInfoPortal(sc,customerData);
         Floor f1 = new Floor(2, 3, 6, 7, 7, 0, 1);
         sc.close();
