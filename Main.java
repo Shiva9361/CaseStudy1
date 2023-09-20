376666247fd43977b55099820d54f4d64616eacf
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-21 00:37:00 +0530

    Modified main

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
         customerData[0] = new Customer("a", "9999999999", null, 0, 0,"aa");
         customerData[1] = new Customer("b", "8888888888", null, 0, 0,"bb");
         customerData[2] = new Customer("c", "0000000000", null, 0, 0,"cc");// add some id
-        */
+        */ 
         ArrayList<Ticket> tickets = new ArrayList<Ticket>();
         //CustomerInfoPortal cip = new CustomerInfoPortal(sc,customerData);
         Floor f1 = new Floor(2, 3, 6, 7, 7, 0, 1);
