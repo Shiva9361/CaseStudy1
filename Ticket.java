6d5f0f957693577ff7ba589cd535a6ae339db34c
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-21 12:29:38 +0530

    Should modify to have parking type

--- Ticket.java
+++ Ticket.java
@@ -3,17 +3,20 @@ import java.util.UUID; // TO create unique id for tickets
 public class Ticket {
     String ticketId;
     String customerId;//null if customer is not registered
-    String phoneNumber;// if customer is not registered then phone number shall be used 
+    String phoneNumber;// if customer is not registered then phone number shall be used
+    String parkingType; // add parking type 
     long entryTime;
     
-    Ticket(ArrayList<Ticket> Tickets,String customerId){
-        this.ticketId = UUID.randomUUID().toString();
+    Ticket(ArrayList<Ticket> Tickets,String customerId,String parkingType){
+
+        this.ticketId = UUID.randomUUID().toString().substring(0, 7);
         this.customerId=customerId;
         this.entryTime = System.currentTimeMillis();
+        
         Tickets.add(this);
     }
     Ticket(String phoneNumber,ArrayList<Ticket> Tickets){
-        this.ticketId = UUID.randomUUID().toString();
+        this.ticketId = UUID.randomUUID().toString().substring(0, 7);
         this.phoneNumber=phoneNumber;
         this.entryTime= System.currentTimeMillis();
         Tickets.add(this);
