fe4c81a5ec33607265ca6c504c1e1e17311ae9fb
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-22 00:07:01 +0530

    Added a initial value to latest ticket id

--- Customer.java
+++ Customer.java
@@ -3,7 +3,7 @@ public class Customer {
     String phoneNumber;
     String address;
     String password;
-    String latestTicketId;
+    String latestTicketId="";
     int accountBalance;
     long creditCardNumber;
     Customer(String customerId,String phoneNumber,String address,int accountBalance,long creditCardNumber,String password){
