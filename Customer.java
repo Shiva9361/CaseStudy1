fee09eec99274c6950aeb2771272fafced8050a9
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-21 21:52:39 +0530

    Added ticket data

--- Customer.java
+++ Customer.java
@@ -3,6 +3,7 @@ public class Customer {
     String phoneNumber;
     String address;
     String password;
+    String latestTicketId;
     int accountBalance;
     long creditCardNumber;
     Customer(String customerId,String phoneNumber,String address,int accountBalance,long creditCardNumber,String password){
