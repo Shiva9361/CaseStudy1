29f8d94570087221d3ddce79c0f748db6da0d0e1
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-21 21:51:51 +0530

    Modified constructor to add ticked id to customer

--- Ticket.java
+++ Ticket.java
@@ -4,21 +4,30 @@ public class Ticket {
     String ticketId;
     String customerId;//null if customer is not registered
     String phoneNumber;// if customer is not registered then phone number shall be used
-    String parkingType; // add parking type 
+    int parkingType; // 1,2,3,4 corresponding to the choice in the menu
     long entryTime;
     
-    Ticket(ArrayList<Ticket> Tickets,String customerId,String parkingType){
+    Ticket(ArrayList<Ticket> Tickets,String customerId,int parkingType,ArrayList<Customer> customers){
 
         this.ticketId = UUID.randomUUID().toString().substring(0, 7);
         this.customerId=customerId;
         this.entryTime = System.currentTimeMillis();
-        
+        this.parkingType=parkingType;
         Tickets.add(this);
+
+        for(Customer c:customers){
+            if(customerId.compareTo(c.customerId)==0){
+                c.latestTicketId=this.ticketId; // Adding the latest ticket into customer 
+                // We are assuming that people always pay when they leave and don't cheat
+                // Futher we assume that a person won't come and park multiple vehicles at the same time 
+            }
+        }
     }
-    Ticket(String phoneNumber,ArrayList<Ticket> Tickets){
+    Ticket(String phoneNumber,ArrayList<Ticket> Tickets,int parkingType){
         this.ticketId = UUID.randomUUID().toString().substring(0, 7);
         this.phoneNumber=phoneNumber;
         this.entryTime= System.currentTimeMillis();
+        this.parkingType=parkingType;
         Tickets.add(this);
     }
 }
