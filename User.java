f4d862d9da7a6c052c0c1b960f6646928d7df9bb
Ikshitha1004 <131011355+Ikshitha1004@users.noreply.github.com>
2023-09-21 13:45:23 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,11 @@
-public class User {
+public class User extends CustomerInfoPortal {
+    // user only uses it for paying at the customerinfo portal
+    User(){
+   super();
+    }
+     
+    
+    
+
     
 }
