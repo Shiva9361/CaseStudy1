2535db44aec2fc4f38b8e594d0a1088b6747941b
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-21 22:16:35 +0530

    modified

--- Main.java
+++ Main.java
@@ -9,25 +9,23 @@ public class Main {
         customerData.add(new Customer("a", "9999999999", null, 0, 0,"aa"));
         customerData.add(new Customer("b", "8888888888", null, 0, 0,"bb"));
         customerData.add(new Customer("c", "0000000000", null, 0, 0,"cc"));
-        /*
-        customerData[0] = new Customer("a", "9999999999", null, 0, 0,"aa");
-        customerData[1] = new Customer("b", "8888888888", null, 0, 0,"bb");
-        customerData[2] = new Customer("c", "0000000000", null, 0, 0,"cc");// add some id
-        */ 
+        
         ArrayList<Ticket> tickets = new ArrayList<Ticket>();
-        //CustomerInfoPortal cip = new CustomerInfoPortal(sc,customerData);
         Floor f1 = new Floor(2, 3, 6, 7, 7, 0, 1);
         Floor.EntryPoint e1 = f1.entryPoints[0];
         Floor.EntryPoint e2 = f1.entryPoints[1];  
         Floor.ExitPoint E1 = f1.exitPoints[0];  
 
         // Simulating generation of ticket in 2 entry points
-        System.out.println("Your ticket ID: "+e1.generateTicket(sc, tickets));
-        System.out.println("Your ticket ID: "+e2.generateTicket(sc, tickets));
+        System.out.println("Your ticket ID: "+e1.generateTicket(sc, tickets,customerData));
+        System.out.println("Your ticket ID: "+e2.generateTicket(sc, tickets,customerData));
         
         // Simulating payment in one exit using both methods
         E1.payment(sc,00000000000000000, tickets);
-        E1.payment(1000, sc, tickets);
+
+        //Simulating customer info portal
+        new CustomerInfoPortal(sc,customerData,tickets);
+        
 
         sc.close();
     }
