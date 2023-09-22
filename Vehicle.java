9f364eb4d7705be683611b07df83acb27b430245
Ikshitha1004 <131011355+Ikshitha1004@users.noreply.github.com>
2023-09-22 16:23:14 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,12 +1,58 @@
 // dk if this is required 
-
-public class Vehicle {
-    int size;
-    String vehicleid;
-    
+abstract class vehicle {
+   String id;
+  String typeofVehicle;
+  String typeofSpace;
 
 }
-class electricVehicle extends Vehicle{
+
+class electricVehicle extends vehicle {
+    electricVehicle(){
+    this.id="";
+    this.typeofVehicle="electric";
+   this.typeofSpace="";
+    }
+
     int charge;
+}
+class car extends vehicle{
+    car(){
+    typeofVehicle="car";
+     id="";
+    
+    this.typeofSpace="";
+    }
+
+}
+class bus extends vehicle{
+   bus(){
+     typeofVehicle="car";
+    typeofVehicle="car";
+     id="";
+    this.typeofVehicle="electric";
+    this.typeofSpace="";
+   }
+}
+class truck extends vehicle{
+    truck(){
+
+    
+     typeofVehicle="car";
+     id="";
+    this.typeofVehicle="electric";
+    this.typeofSpace="";
+    }
     
 }
+class bike extends vehicle{
+bike(){
+
+    
+     typeofVehicle="car";
+     id="";
+    this.typeofVehicle="electric";
+    this.typeofSpace="";
+    }
+}
+
+
