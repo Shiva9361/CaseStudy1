3db586408e02248cdb39f53e2f597e1952499124
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-22 20:22:19 +0530

    Added getters and setters

--- Customer.java
+++ Customer.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+
 public class Customer {
     String customerId;
-    String phoneNumber;
-    String address;
-    String password;
-    String latestTicketId="";
-    int accountBalance;
-    long creditCardNumber;
+    private String phoneNumber;
+    private String address;
+    private String password;
+    private String latestTicketId="";
+    private int accountBalance;
+    private long creditCardNumber;
     Customer(String customerId,String phoneNumber,String address,int accountBalance,long creditCardNumber,String password){
         this.customerId=customerId;
         this.phoneNumber=phoneNumber;
@@ -13,5 +15,34 @@ public class Customer {
         this.accountBalance=accountBalance;
         this.creditCardNumber=creditCardNumber;
         this.password=password;
+    }
+    static boolean customerExists(String customerId,ArrayList<Customer>customers){
+        for(Customer customer:customers ){
+            if(customer.customerId.compareTo(customerId)==0){
+                return true;
+            }
+        }
+        return false;
+    }
+    String phonenumberGetter(){
+        return phoneNumber;
+    }
+    int accountBalanceGetter(){
+        return accountBalance;
+    }
+    String addressGetter(){
+        return address;
+    }
+    String passwordGetter(){
+        return this.password;
+    }
+    long creditCardNumberGetter(){
+        return this.creditCardNumber;
+    }
+    String latestTicketIdGetter(){
+        return this.latestTicketId;
+    }
+    void latestTicketIdSetter(String latestTicketId){
+        this.latestTicketId=latestTicketId;
     }
 }
