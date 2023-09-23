f68df1f85953dcf02877e400f410e3247784c839
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-23 13:01:56 +0530

    Check  This

--- Person.java
+++ Person.java
@@ -1,37 +1,46 @@
-public class Person{
+import java.util.Scanner;
+
+abstract class Person{
      int  id;
      String name;
      String address;
 
 }
+
 class Employee extends Person{
+    int shiftfortheday=1;
+    boolean presence=true;
+    
     Employee(String name,int id){
         this.id=id;
         this.name=name;
     }
-        int shiftfortheday=1;
-        boolean presence=true;
-
 }
+
 class Admin extends Person{
-    void changeSlots(String Allotment[][],Scanner sc,int floornumber){
-        System.out.println("Enter the slot code for whatever slot you want to change");
-        String p=sc.next();
-        System.out.println("the available slots are ");
-       
-      int n=Allotment.length;
+    Admin(){
+
+    }
+    void changeSlots(String Allotment[][],Scanner sc,Floor floor){
+        boolean isvalidSlotcode=false;
+        String slot="";
+        while(!isvalidSlotcode){
+            System.out.println("Enter the slot code for whatever slot you want to change");
+            slot=sc.next();
+            System.out.println("The available slots are ");
+
+        }
+
+        int n=Allotment.length;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                   if(Allotment[i][j].split(" ")[1]=="false"){
-                         Allotment[i][j]=floornumber+p+"temp"+" "+"true";
-                         break;
-                   }
+                if(Allotment[i][j].split(" ")[1]=="false"){
+                    Allotment[i][j]=floor.floorNumber+p+"temp"+" "+"true";
+                    break;
+                }
             }
-
         }
        
 
     };
-    //to be able to change the slots
-
 }
