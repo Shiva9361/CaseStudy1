a31341d6d8fe169ab205002c6868d194c268622a
Ikshitha1004 <131011355+Ikshitha1004@users.noreply.github.com>
2023-09-23 10:16:17 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,22 @@
+public class Person{
+     int  id;
+     String name;
+     String address;
+
+}
+class Employee extends Person{
+    Employee(String name,int id){
+        this.id=id;
+        this.name=name;
+    }
+        int shiftfortheday=1;
+        boolean presence=true;
+
+}
+class Admin extends Person{
+    void changeSlots(String p){
+        
+    };
+    //to be able to change the slots
+
+}
