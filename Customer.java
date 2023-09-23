601cbc12898e7982fba8a61d10c6d3a1c925ad8d
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-23 12:49:03 +0530

    Added comments

--- Customer.java
+++ Customer.java
@@ -1,3 +1,7 @@
+/*
+ * Implementing customer as a class, Whenever a customer registers themselves in the customer info portal,
+ * A customer object is created and this object is pushed into the customer array list.
+ */
 import java.util.ArrayList;
 
 public class Customer {
@@ -7,7 +11,7 @@ public class Customer {
     private String password;
     private String latestTicketId="";
     private int accountBalance;
-    private long creditCardNumber;
+    private long creditCardNumber=0;
     Customer(String customerId,String phoneNumber,String address,int accountBalance,long creditCardNumber,String password){
         this.customerId=customerId;
         this.phoneNumber=phoneNumber;
@@ -16,6 +20,7 @@ public class Customer {
         this.creditCardNumber=creditCardNumber;
         this.password=password;
     }
+    //For a given customer ID, check if it is valid
     static boolean customerExists(String customerId,ArrayList<Customer>customers){
         for(Customer customer:customers ){
             if(customer.customerId.compareTo(customerId)==0){
@@ -24,6 +29,7 @@ public class Customer {
         }
         return false;
     }
+    //As we don't want customer info to be manipulted by external forces, Everything is made private and getter methods are used
     String phonenumberGetter(){
         return phoneNumber;
     }
@@ -42,6 +48,7 @@ public class Customer {
     String latestTicketIdGetter(){
         return this.latestTicketId;
     }
+    //Setter method is used to change the lastest tickey ID
     void latestTicketIdSetter(String latestTicketId){
         this.latestTicketId=latestTicketId;
     }
