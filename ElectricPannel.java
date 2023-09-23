fc167046a63a89344b65ed932f21fddc5c7d7733
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-23 19:27:43 +0530

    Bug fix

--- ElectricPannel.java
+++ ElectricPannel.java
@@ -1,3 +1,6 @@
+/*
+ * Implemmenting Electric pannel as a class which is intantiated as an object of each floor
+ */
 import java.util.Scanner;
 public class ElectricPannel {
     ElectricPannel(Scanner sc){
@@ -5,9 +8,10 @@ public class ElectricPannel {
         menu(sc);
     }
     private void menu(Scanner sc){
-        System.out.println("1:Pay and refill\n0:Quit");
         boolean quit = false;
         while(!quit){
+            System.out.println();
+            System.out.println("1:Pay and refill\n0:Quit");
             String choice = sc.nextLine();
             if (choice.compareTo("1")==0){
                 refill(sc);
@@ -18,15 +22,25 @@ public class ElectricPannel {
             else{
                 System.out.println("Invalid Choice");
             }
-            System.out.println();
-            System.out.println("1:Pay and refill\n0:Quit");
+
         }
-    }private void refill(Scanner sc){
+    }
+    /*
+     * Only one option is available which is to pay and refill
+     * If given more time, Can implement cutomer id integration 
+     */
+    private void refill(Scanner sc){
         System.out.println("Enter credit card number: ");
-        System.out.println("Enter Amount to refill in Rupees\n(Standard Electricity converion rates apply)");
-        int amount = sc.nextInt();sc.nextLine();
-        //  assuming credit card is valid and people don't cheat
-        // assuming payment is being handled by a backend software
-        System.out.println("Done recharging for "+amount);
+        long creditCardNumber=sc.nextLong();sc.nextLine();
+        if (creditCardNumber!=0){
+            System.out.println("Enter Amount to refill in Rupees\n(Standard Electricity converion rates apply)");
+            int amount = sc.nextInt();sc.nextLine();
+            // assuming credit card is valid and people don't cheat
+            // assuming payment is being handled by a backend software
+            System.out.println("Done recharging for "+amount);
+        }
+        else{
+            System.out.println("No credit card number given..");
+        }
     }
 }
