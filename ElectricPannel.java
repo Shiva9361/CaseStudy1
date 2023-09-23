a4e9d86c674f375b9a38830ef90fe22996ec719e
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-23 12:49:37 +0530

    Added comments

--- ElectricPannel.java
+++ ElectricPannel.java
@@ -1,9 +1,17 @@
+/*
+ * Implemmenting Electric pannel as a class which is intantiated as an object of each floor
+ */
 import java.util.Scanner;
 public class ElectricPannel {
+    //Parameterised constructor 
     ElectricPannel(Scanner sc){
         System.out.println("Electric Pannel");
         menu(sc);
     }
+    /*
+     * Only one option is available which is to pay and refill
+     * If given more time, Can implement cutomer id integration 
+     */
     private void menu(Scanner sc){
         System.out.println("1:Pay and refill\n0:Quit");
         boolean quit = false;
@@ -24,10 +32,15 @@ public class ElectricPannel {
     }private void refill(Scanner sc){
         System.out.println("Enter credit card number: ");
         long creditCardNumber=sc.nextLong();sc.nextLine();
-        System.out.println("Enter Amount to refill in Rupees\n(Standard Electricity converion rates apply)");
-        int amount = sc.nextInt();sc.nextLine();
-        //  assuming credit card is valid and people don't cheat
-        // assuming payment is being handled by a backend software
-        System.out.println("Done recharging for "+amount);
+        if (creditCardNumber!=0){
+            System.out.println("Enter Amount to refill in Rupees\n(Standard Electricity converion rates apply)");
+            int amount = sc.nextInt();sc.nextLine();
+            // assuming credit card is valid and people don't cheat
+            // assuming payment is being handled by a backend software
+            System.out.println("Done recharging for "+amount);
+        }
+        else{
+            System.out.println("No credit card number given..");
+        }
     }
 }
