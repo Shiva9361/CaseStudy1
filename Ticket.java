53180bc00974a6bb52c22c9c36c63be217023da8
Shiva936 <shivadharshansankar936@gmail.com>
2023-09-23 12:50:10 +0530

    Added comments

--- Ticket.java
+++ Ticket.java
@@ -1,18 +1,26 @@
+/*
+ * Implementing Ticket as a class,
+ * This class is used to generate a ticket object.
+ * Constructor overloading is done to achieve ticket generation in two senarios
+ * Case 1) Customer is registered and uses his customer ID
+ * Case 2) Customer is not registered and uses his phone number   
+ */
 import java.util.ArrayList;
-import java.util.UUID; // TO create unique id for tickets
+import java.util.UUID; // To create unique id for tickets
 public class Ticket {
-    String ticketId;
-    String customerId;//null if customer is not registered
+    String ticketId;// Unique id for each ticket
+    String customerId; // null if customer is not registered
     String phoneNumber;// if customer is not registered then phone number shall be used
-    String parkingType; // 1,2,3,4 corresponding to the choice in the menu
+    String parkingType; // refers to the four major types of parking, namely compact,large,electric and hadicap
     long entryTime;
     Vehicle vehicle;
     
+    //Constructor for cutomer using their ID 
     Ticket(ArrayList<Ticket> Tickets,String customerId,String parkingType,ArrayList<Customer> customers,Vehicle vehicle){
 
         this.ticketId = UUID.randomUUID().toString().substring(0, 7);
         this.customerId=customerId;
-        this.entryTime = System.currentTimeMillis();
+        this.entryTime = System.currentTimeMillis();// Time is recorded at the time of generation of ticket
         this.parkingType=parkingType;
         this.vehicle=vehicle;
         Tickets.add(this);
@@ -25,6 +33,7 @@ public class Ticket {
             }
         }
     }
+    // Constructor for Customer using just phone number
     Ticket(String phoneNumber,ArrayList<Ticket> Tickets,String parkingType,Vehicle vehicle){
         this.ticketId = UUID.randomUUID().toString().substring(0, 7);
         this.phoneNumber=phoneNumber;
